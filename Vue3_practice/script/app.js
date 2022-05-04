ae13d22ed70a0b8be16168ac073ce2a94973155f
jay183145 <jay183145@gmail.com>
2022-05-04 17:16:00 +0800

    Vue3 data

--- Vue3_practice/script/app.js
+++ Vue3_practice/script/app.js
@@ -1,5 +1,10 @@
 const app = Vue.createApp({
-    template: "<h2>I am the template</h2>"
+    // template: "<h2>I am the template</h2>"
+    data() {
+        return {
+            name: 'Kyle'
+        }
+    }
 })
 
 app.mount('#app')
